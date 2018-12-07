3ff50bbc1e28cceac1dc7e862c8e4293491d0605
Armand Zentil <armandzentil@gmail.com>
2018-12-06 21:29:14 -0500

    added filter bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import FilterBar from './components/FilterBar';
+
 
 class App extends Component {
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FilterBar from './FilterBar'
 
 function Header(props) {
 	return (
 		<header>
 		  <h1>{props.title}</h1>
 		  <p>{props.message}</p>
+		  <FilterBar />
 		</header>
 	);
 };
